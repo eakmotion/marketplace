fe588004d2be4f03f8d220d334b9cf322c739e1d
eakmotion <eakpun@gmail.com>
2015-03-04 20:54:05 +0700

    Setup paperclip dropbox gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,12 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
+  end
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
