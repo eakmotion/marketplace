96c6c0da5728434080cf9987ecbabffd530fadf4
eakmotion <eakpun@gmail.com>
2015-03-05 01:41:45 +0700

    Transfer funds to recipient

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,13 @@ class OrdersController < ApplicationController
       flash[:danger] = e.message
     end
 
+    # Charge 5% fees
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
+
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_url }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304164120) do
+ActiveRecord::Schema.define(version: 20150304181903) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150304164120) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
