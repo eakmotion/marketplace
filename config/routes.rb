78224da0bb855ff35fe4f72b06703a136dd80b4d
eakmotion <eakpun@gmail.com>
2015-03-05 00:30:13 +0700

    Setup sales and purchases page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  get 'seller' => "listings#seller"
   get 'pages/about'
   get 'pages/contact'
+  get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
   root 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
